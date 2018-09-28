5ccf7bb0514fb0b543e6f7695f5b683131194199
laszlo-pfeifer <pfeiferlaszlo@gmail.com>
2018-09-28 12:19:18 +0200

    logs sleep interval added

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ You can run the dummy app with the following command:
 node index.js
 ```
 
-You can customize the number of lines with the following environment variables:
+You can customize the number of lines and the sleep interval with the following environment variables:
 ```
 MIN_NUMBER_OF_LINES=1 MAX_NUMBER_OF_LINES=4 node index.js
 ```
@@ -15,3 +15,4 @@ MIN_NUMBER_OF_LINES=1 MAX_NUMBER_OF_LINES=4 node index.js
 The default values are:
 MIN_NUMBER_OF_LINES = 10
 MAX_NUMBER_OF_LINES = 30
+SLEEP_INTERVAL_BETWEEN_LINES_IN_MS = 500
--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 
 const minNumberOfLines = parseInt(process.env.MIN_NUMBER_OF_LINES) || 10
 const maxNumberOfLines = parseInt(process.env.MAX_NUMBER_OF_LINES) || 30
+const sleepInterval = parseInt(process.env.SLEEP_INTERVAL_BETWEEN_LINES_IN_MS) || 500
 const numberOfLines = minNumberOfLines + Math.floor(Math.random() * Math.floor(maxNumberOfLines- minNumberOfLines))
 
 const logs = Array(numberOfLines).fill().map((_, i) => `${i + 1 }. line: ${Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}`)
 
-logs.forEach(l => console.log(l))
+print(logs)
+
+async function print(logs) {
+    for (const line of logs) {
+        await sleep()
+        console.log(line)
+    }
+}
+
+async function sleep() {
+    return new Promise((res, _) => {
+        setTimeout(res, sleepInterval)
+    })
+}
+
+  
